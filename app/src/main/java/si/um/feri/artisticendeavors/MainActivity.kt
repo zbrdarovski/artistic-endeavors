1530027ca6e64578d82a9271a95e9b3749e3646d
zdravko.brdarovski <zdravko.brdarovski@student.um.si>
2023-04-19 21:28:32 +0200

    Save username and photo

--- app/src/main/java/si/um/feri/artisticendeavors/MainActivity.kt
+++ app/src/main/java/si/um/feri/artisticendeavors/MainActivity.kt
@@ -2,29 +2,85 @@ package si.um.feri.artisticendeavors
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import android.widget.Toast
-import androidx.activity.ComponentActivity
+import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.UserProfileChangeRequest
+import com.squareup.picasso.Picasso
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.withContext
+import si.um.feri.artisticendeavors.databinding.ActivityMainBinding
 
-class MainActivity : ComponentActivity() {
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    val auth = FirebaseAuth.getInstance()
+    val user = auth.currentUser
+
+    companion object {
+        val IMAGE_REQUEST_CODE = 1_000;
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val action_signout: Button = findViewById(R.id.action_signout)
+        binding.change.setOnClickListener {
+            pickImageFromGallery()
+        }
 
-        action_signout.setOnClickListener {
-            FirebaseAuth.getInstance().signOut()
+        binding.usern.text = user?.displayName
+        Picasso.get().load(user?.photoUrl).into(binding.profpic)
 
-            Toast.makeText(
-                this@MainActivity,
-                "You logged out successfully.",
-                Toast.LENGTH_SHORT
-            ).show()
+        binding.actionSignout.setOnClickListener {
+                auth.signOut()
 
-            startActivity(Intent(this@MainActivity, LoginActivity::class.java))
-            finish()
+                Toast.makeText(
+                    this@MainActivity,
+                    "You logged out successfully.",
+                    Toast.LENGTH_SHORT
+                ).show()
+
+                startActivity(Intent(this@MainActivity, LoginActivity::class.java))
+                finish()
+            }
+        }
+    private fun pickImageFromGallery() {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent, IMAGE_REQUEST_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == IMAGE_REQUEST_CODE && resultCode == RESULT_OK) {
+            val uri = data?.data
+            binding.profpic.setImageURI(uri)
+            user?.let { user ->
+                val profileUpdates = UserProfileChangeRequest.Builder()
+                    .setPhotoUri(uri)
+                    .build()
+
+                CoroutineScope(Dispatchers.IO).launch {
+                    try {
+                        user.updateProfile(profileUpdates).await()
+                        withContext(Dispatchers.Main) {
+                            Toast.makeText(this@MainActivity, "Successfully updated profile",
+                                Toast.LENGTH_LONG).show()
+                        }
+                    } catch(e: Exception) {
+                        withContext(Dispatchers.Main) {
+                            Toast.makeText(this@MainActivity, e.message, Toast.LENGTH_LONG).show()
+                        }
+                    }
+
+                }
+            }
         }
     }
 }
--- app/src/main/java/si/um/feri/artisticendeavors/RegisterActivity.kt
+++ app/src/main/java/si/um/feri/artisticendeavors/RegisterActivity.kt
@@ -10,16 +10,27 @@ import android.widget.Toast
 import androidx.activity.ComponentActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.UserProfileChangeRequest
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.withContext
+
 
 class RegisterActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
 
+        val email: EditText = findViewById(R.id.email)
         val username: EditText = findViewById(R.id.username)
         val password: EditText = findViewById(R.id.password)
+        val repeat: EditText = findViewById(R.id.repeat)
         val action: Button = findViewById(R.id.action_register)
         val option: TextView = findViewById(R.id.option)
+
+        val pattern = Regex("^(?=.{8,20}\$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])\$")
 
         option.setOnClickListener {
             val intent = Intent(this@RegisterActivity, LoginActivity::class.java)
@@ -30,11 +41,31 @@ class RegisterActivity : ComponentActivity() {
 
         action.setOnClickListener {
             when {
-                TextUtils.isEmpty(username.text.toString().trim { it <= ' ' }) -> {
+                TextUtils.isEmpty(email.text.toString().trim { it <= ' ' }) -> {
                     Toast.makeText(
                         this@RegisterActivity,
                         "Please enter email.",
                         Toast.LENGTH_SHORT
+                    ).show()
+                }
+
+                TextUtils.isEmpty(username.text.toString().trim { it <= ' ' }) -> {
+                    Toast.makeText(
+                        this@RegisterActivity,
+                        "Please enter username.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+
+                !pattern.containsMatchIn(username.text.toString().trim { it <= ' ' }) -> {
+                    Toast.makeText(
+                        this@RegisterActivity,
+                        "Username is 8-20 characters long." +
+                                "Allowed characters: alphanumeric characters, underscore and dot." +
+                                "No underscore or dot at the end or the beginning." +
+                                "Underscore and dot can't be next to each other." +
+                                "Underscore or dot can't be used multiple times in a row.",
+                        Toast.LENGTH_LONG
                     ).show()
                 }
 
@@ -46,15 +77,51 @@ class RegisterActivity : ComponentActivity() {
                     ).show()
                 }
 
-                else -> {
-                    val email: String = username.text.toString().trim { it <= ' ' }
-                    val pass: String = password.text.toString().trim { it <= ' ' }
+                TextUtils.isEmpty(repeat.text.toString().trim { it <= ' ' }) -> {
+                    Toast.makeText(
+                        this@RegisterActivity,
+                        "Please repeat password.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
 
-                    FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, pass)
+                password.text.toString() != repeat.text.toString() -> {
+                    Toast.makeText(
+                        this@RegisterActivity,
+                        "Passwords don't match.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+
+                else -> {
+                    val em: String = email.text.toString().trim { it <= ' ' }
+                    val pass: String = password.text.toString().trim { it <= ' ' }
+                    val auth = FirebaseAuth.getInstance()
+                    auth.createUserWithEmailAndPassword(em, pass)
                         .addOnCompleteListener { task ->
                             if (task.isSuccessful) {
 
                                 val firebaseUser: FirebaseUser = task.result!!.user!!
+
+                                auth.currentUser?.let { user ->
+                                    val profileUpdates = UserProfileChangeRequest.Builder()
+                                        .setDisplayName(username.text.toString().trim { it <= ' ' }).build()
+
+
+                                    CoroutineScope(Dispatchers.IO).launch {
+                                        try {
+                                            user.updateProfile(profileUpdates).await()
+                                        } catch(e:Exception){
+                                            withContext(Dispatchers.Main) {
+                                            Toast.makeText(
+                                                this@RegisterActivity,
+                                                e.message,
+                                                Toast.LENGTH_LONG
+                                            ).show()
+                                            }
+                                        }
+                                    }
+                                }
 
                                 Toast.makeText(
                                     this@RegisterActivity,
@@ -63,18 +130,18 @@ class RegisterActivity : ComponentActivity() {
                                 ).show()
 
                                 val intent =
-                                    Intent(this@RegisterActivity, MainActivity::class.java)
+                                    Intent(this@RegisterActivity, LoginActivity::class.java)
                                 intent.flags =
                                     Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
                                 intent.putExtra("user_id", firebaseUser.uid)
-                                intent.putExtra("email_id", email)
+                                intent.putExtra("email_id", em)
                                 startActivity(intent)
                                 finish()
                             } else {
                                 Toast.makeText(
                                     this@RegisterActivity,
                                     task.exception!!.message.toString(),
-                                    Toast.LENGTH_SHORT
+                                    Toast.LENGTH_LONG
                                 ).show()
                             }
                         }
